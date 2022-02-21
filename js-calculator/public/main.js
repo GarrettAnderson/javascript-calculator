a6c284050fbecfae3a51ee8004d28ae54806ed98
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-02-21 18:43:15 -0500

    added js to print numbers to the console. added an id to each of the numbers for the js to access them

--- js-calculator/public/main.js
+++ js-calculator/public/main.js
@@ -1,7 +1,13 @@
-function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
-}
+// function main() {
+//   if (document.querySelector('h1.hello-world')) {
+//     document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+//   }
+// }
+//
+// document.addEventListener('DOMContentLoaded', main)
 
-document.addEventListener('DOMContentLoaded', main)
+
+// Collect the numbers and set them to a variable - an array
+
+const numbers = document.querySelectorAll('.numbers')
+console.log(numbers)
