10701ad990bb13017493f0694e04a19596094969
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-02-21 18:59:13 -0500

    added some notes for plan of action. writing up a PEDAC to help with the js code.

--- js-calculator/public/main.js
+++ js-calculator/public/main.js
@@ -9,9 +9,28 @@
 
 // Collect the numbers and set them to a variable - an array
 
+let number
 const numbers = document.querySelectorAll('#number')
 console.log(Array.from(numbers))
 
+// Print the numbers
 for (number in numbers) {
   console.log(number)
+
+  number.addEventListener({
+    console.log{'number clicked'}
+  })
 }
+
+
+// When user clicks on a number - that number gets stored as a variable... firstNum
+  // the value is revealed in the solution box on the UI
+
+// When user clicks on a math operator, the operation method is stored in memory
+  // the value is revealed in the solution box on the UI
+
+// When the user clicks on the second number, that number get stored in a second variable... secondNum
+
+
+// When the user presses the equal sign button, the math operation is calculated
+  // the value is revealed in the solution box on the UI
